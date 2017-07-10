39aaa25bfca84b3def3aca07db34123d51f7f9c9
tutu <tutugamestutubooks@gmail.com>
2017-07-10 14:16:34 +0900

    基本の高階関数

--- highher.js
+++ highher.js
@@ -4,7 +4,7 @@
 }
 高階関数*/
 
-//高階関数arraywalkを定義
+/*//高階関数arraywalkを定義
 function arraywalk(data,f){
     for (let key in data){
         f(data[key],key);
@@ -15,4 +15,18 @@ function showElement(value,key){
     console.log(key + '：' + value);
 }
 let ary = [1,2,4,8,16];
-arraywalk(ary,showElement);
+arraywalk(ary,showElement);*/
+
+
+//高階関数
+function myHigherFunction(func){
+    console.log('miHigherFunction');
+    func();
+}
+　//（普通の）関数
+function outsideFunction(){
+    console.log('outsideFunction');    
+}
+
+outsideFunction();//←関数呼び出し
+myHigherFunction(outsideFunction);//関数渡し
